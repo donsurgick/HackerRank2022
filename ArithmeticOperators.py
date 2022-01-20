6713c668cb9d58efeffb9b2dd90f253bcd005430
donsurgick <97745151+donsurgick@users.noreply.github.com>
2022-01-20 10:36:15 -0500

    Create ArithmeticOperators.py

---
+++ ArithmeticOperators.py
@@ -0,0 +1,13 @@
+#gets user input from two variables
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+#prints the sum of two integers   
+    ans = a + b
+    print (ans)
+#prints the difference of two integers    
+    ans = a - b
+    print (ans)
+#prints the product of two integers    
+    ans = a * b
+    print (ans)
